c6617f0bf486f23f5bb7abd6c5c4eeaed6b3f852
uvok <gh@uvokchee.de>
2021-11-27 16:16:38 +0100

    Add name test

--- rotkehlchen/tests/exchanges/test_bitpandapro.py
+++ rotkehlchen/tests/exchanges/test_bitpandapro.py
@@ -7,3 +7,9 @@ import pytest
 import requests
 
 from rotkehlchen.exchanges.bitpandapro import BitpandaPro
+from rotkehlchen.typing import Location
+
+def test_name():
+    exchange = BitpandaPro('bitpandapro1', 'a', b'a', object(), object())
+    assert exchange.location == Location.BITPANDAPRO
+    assert exchange.name == 'bitpandapro1'
